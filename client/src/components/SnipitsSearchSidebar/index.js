b82c8aa74a0176b09843553d9a647f08b2d6526a
Sean Kirkpatrick <Skirkp18@gmail.com>
2021-01-18 22:09:47 -0500

    added tags to sidebar of mainpage

--- client/src/components/SnipitsSearchSidebar/index.js
+++ client/src/components/SnipitsSearchSidebar/index.js
@@ -5,9 +5,22 @@ import Card from 'react-bootstrap/Card';
 function SnipitsSearchSidebar() {
 
     return(		<Card id="snipitsPageSideBar">
-    <Card.Header><h1>Search Snipits</h1></Card.Header>
-    <Card.Body>This is just some test text to make sure that this is working!</Card.Body>
-    <Card.Footer>Filter Snipits</Card.Footer>
+    <Card.Header><h1>Search And Filter Snipits</h1></Card.Header>
+    <Card.Body style={{backgroundColor: "blue"}}>
+        <h2>Categories:</h2>
+        <ul>
+            <a href=""><li>Javascript</li></a>
+            <br />
+            <a href=""><li>HTML</li></a>
+            <br />
+            <a href=""><li>CSS</li></a>
+            <br />
+            <a href=""><li>JSX</li></a>
+            <br />
+            <a href=""><li>Python</li></a>
+        </ul>
+        </Card.Body>
+    <Card.Footer><a href=""><p>Reset Filters</p></a></Card.Footer>
 </Card>);
 };
 
